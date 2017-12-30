61dea219c31af33e4abed04d70c6fb4bc0e68a8d
guigui2707 <guigui2707@live.fr>
2017-12-30 16:39:52 +0100

    micro_blog2
    Ajout de la librairie Smarty + finalisation des cookies de session

--- includes/connexion.inc.php
+++ includes/connexion.inc.php
@@ -1,3 +1,25 @@
 <?php
-$pdo=new PDO('mysql:host=localhost;dbname=micro_blog','root','');
+/*CONNEXION A LA BASE DE DONNEES*/
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=micro_blog2', 'root', '');
+
+//On crée le sid de l'utilisateur en fonction de son cookie 
+if(isset($_COOKIE['cookieUtilisateur'])){
+	$query='SELECT * FROM utilisateurs WHERE sid=:sid';
+	$prep= $pdo->prepare($query);
+	$prep->bindValue(':sid', $_COOKIE['cookieUtilisateur']);
+	$prep->execute();
+	$count=$prep->rowCount();
+	if($count!=0){
+		while ($data = $prep->fetch()) {
+			$nom=$data['nom'];
+		}
+	}
+	else{
+		$nom="";
+	}
+}
+//Si le cookie n'existe pas, on crée une variable $nom 
+else{
+	$nom="";
+}
 ?>
