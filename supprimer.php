4ee177f515acdd893cab53e5a1ec9e252e9ac2fc
guigui2707 <guigui2707@live.fr>
2017-12-31 15:57:37 +0100

    modif cookie +tpl
    footer du site converti en smarty et modification de la partie message pour inclure l'expiration du cookie en cas de modif

--- supprimer.php
+++ supprimer.php
@@ -1,24 +1,8 @@
 <?php
 include('includes/connexion.inc.php');
-
-$a= $_GET['a'];
-
-
-if ($a=='sup'){
-
-$sql="DELETE FROM messages WHERE id=:id";
-$prep=$pdo->prepare($sql);
-$prep->bindValue(':id', $_GET['id']);
+//Supprimer le message 
+$sql='DELETE FROM messages WHERE id='.$_GET['id'];
+$prep = $pdo->prepare($sql);
 $prep->execute();
-header("Location:index.php");
-exit();
-    
-}else{
-$sql=" INSERT INTO messages (contenu,date) VALUES (:contenu,UNIX_TIMESTAMP())";
-$prep=$pdo->prepare($sql);
-$prep->bindValue(':contenu', $_POST['message']);
-$prep->execute();
-}
-
-
+header('Location:index.php');
 ?>
